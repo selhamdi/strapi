2de414d305fcf1d182584cb40a15996c3df422f0
salima <salimaeii1997@gmail.com>
2021-05-27 20:00:37 +0100

    done

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e6108ee82ca03554942c4f35d305fcf7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '710eff16-cf86-44fa-aa71-b03d1772bba0'
+};
